6cd31b5bd503ac4756779eec5066db16fe9b4380
Lionel Chun <lionel.chun@outlook.com>
2023-11-12 23:16:47 +0800

    Beta 2

--- BCRE/settings.py
+++ BCRE/settings.py
@@ -90,7 +90,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'bcre', 
         'USER': 'postgres',
-        'PASSWORD': 'Lionel@1978',
+        'PASSWORD': 'Donbe@0919',
         'HOST': '127.0.0.1', 
         'PORT': '5432',
     }
